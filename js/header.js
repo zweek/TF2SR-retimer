1273d3465f784548fec8e037160d5d3089a1da1b
Lucas Henneke <70382336+zweek@users.noreply.github.com>
2022-01-19 23:20:00 +0100

    add local storage

--- js/header.js
+++ js/header.js
@@ -1,4 +1,5 @@
 const categorySelect = document.querySelector("#category-select")
+const includeMScheckbox = document.querySelector("#includeMS-checkbox")
 
 function clearContent() {
     // delete all level elems (but keep the template)
@@ -17,20 +18,23 @@ function contentReset() {
 
 categorySelect.onchange = function() {
     contentReset()
+    localStorage.setItem("settingCategory", categorySelect.value)
 }
 
 document.querySelector("#reset-button").onclick = function() {
     contentReset()
 }
 
+// make local storage var for checkbox, update on click
 function getIncludeMS() {
-    return document.querySelector("#includeMS-checkbox").checked
+    return includeMScheckbox.checked
 }
 
 document.querySelector(".include-ms").onclick = function() {
     
     if (getIncludeMS() === false) {
-        document.querySelector("#includeMS-checkbox").checked = true
+        includeMScheckbox.checked = true
+        localStorage.setItem("settingMSchecked", 1)
         
         const msTemplate = document.querySelector(".segment-container.template .input.ms")
         // add ms input boxes
@@ -38,13 +42,19 @@ document.querySelector(".include-ms").onclick = function() {
             getSegmentElems()[i].querySelector(".start").appendChild(msTemplate.cloneNode(true))
             getSegmentElems()[i].querySelector(".end").appendChild(msTemplate.cloneNode(true))
         }
-
+        
     } else {
-        document.querySelector("#includeMS-checkbox").checked = false
+        includeMScheckbox.checked = false
+        localStorage.setItem("settingMSchecked", "")
         
         // remove ms input boxes
         for (let i = 0; i < getSegmentElems().length * 2; i++) {
             document.querySelector(".segment-container:not(.template) .ms").remove()
         }
     }
+}
+
+includeMScheckbox.checked = localStorage.getItem("settingMSchecked")
+if (localStorage.getItem("settingCategory")) {
+    categorySelect.value = localStorage.getItem("settingCategory")
 }
